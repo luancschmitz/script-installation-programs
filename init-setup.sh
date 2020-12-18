f47c5475fbb698967ff1463c0dab708828f86e04
Luan Carlos Schmitz <luan_c_schmitz@outlook.com>
2020-12-18 10:17:33 -0300

    Update init-setup.sh

--- init-setup.sh
+++ init-setup.sh
@@ -1,49 +1,22 @@
 #!/bin/bash
 
 #INSTALACAO E CONFIGURAÇÃO DO PHP E XDEBUG
-echo "Instalação PHP 5.6 7.0 7.1 E 7.2 com configuração do XDEBUG"
+echo "Instalação PHP 7.3 com configuração do XDEBUG"
 sleep 2s
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt update -y
-sudo apt install -y php7.2 php7.2-fpm php7.1 php7.1-fpm php7.0 php7.0-fpm php5.6 php5.6-fpm
-sudo apt install -y php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-bcmath php7.2-xml php7.2-zip php7.2-intl php7.2-soap php7.2-mysql
-sudo apt install -y php7.1-curl php7.1-gd php7.1-json php7.1-mbstring php7.2-bcmath php7.1-mcrypt php7.1-xml php7.1-zip php7.1-intl php7.1-soap php7.1-mysql
-sudo apt install -y php7.0-curl php7.0-gd php7.0-json php7.0-mbstring php7.0-bcmath php7.0-mcrypt php7.0-xml php7.0-zip php7.0-intl php7.0-soap php7.0-mysql
-sudo apt install -y php5.6-curl php5.6-gd php5.6-json php5.6-mbstring oho5.6-bcmath php5.6-mcrypt php5.6-xml php5.6-zip php5.6-intl php5.6-soap php5.6-mysql
+sudo apt install -y php7.3 php7.3-fpm
+sudo apt install -y php7.3-curl php7.3-gd php7.3-json php7.3-mbstring php7.3-bcmath php7.3-xml php7.3-zip php7.3-intl php7.3-soap php7.3-mysql
 sudo apt install -y php-xdebug
 
 sudo su << SUDO_COMANDS
-echo "xdebug.remote_enable=on" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_autostart=1" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_mode=req" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-echo "xdebug.idekey=phpstorm" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_port=9001" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-echo "xdebug.max_nesting_level=400" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_host=127.0.0.1" >> /etc/php/5.6/fpm/conf.d/20-xdebug.ini
-
-echo "xdebug.remote_enable=on" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_autostart=1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_mode=req" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-echo "xdebug.idekey=phpstorm" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_port=9001" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-echo "xdebug.max_nesting_level=400" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_host=127.0.0.1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-
-echo "xdebug.remote_enable=on" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_autostart=1" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_mode=req" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.idekey=phpstorm" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_port=9001" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.max_nesting_level=400" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_host=127.0.0.1" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-
-echo "xdebug.remote_enable=on" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_autostart=1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_mode=req" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.idekey=phpstorm" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_port=9001" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.max_nesting_level=400" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_host=127.0.0.1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_enable=on" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_autostart=1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_mode=req" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.idekey=phpstorm" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_port=9001" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.max_nesting_level=400" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_host=127.0.0.1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
 SUDO_COMANDS
 
 #INSTALACAO OUTROS PROGRAMAS
@@ -150,13 +123,6 @@ wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_curre
 sudo dpkg -i chrome.deb
 sudo rm -r ~/Downloads/chrome.deb
 
-#INSTALAÇÃO N98 MAGERUN
-echo "Installing n98 Magerun"
-sleep 2s
-curl -O https://files.magerun.net/n98-magerun.phar
-chmod +x ./n98-magerun.phar
-sudo mv ./n98-magerun.phar /usr/local/bin/
-
 # #ISNTALAÇÃO N98 MAGERUN2
 echo "Installing n98 magerun2"
 sleep 2s
@@ -178,12 +144,3 @@ sleep 2s
 curl -sS https://accounts.magento.cloud/cli/installer | php
 source $HOME/.bashrc
 echo "Installation finish"
-
-#INSTALL YARM FRONT-END
-cd /home/$USER
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-sudo su << SUDO_COMANDS
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-SUDO_COMANDS
-sudo apt-get update
-sudo apt-get install yarn
